c7c89eb888d88e4971a01eda373b2c827fc88df4
Jude Okagu <juud0010@gmail.com>
2020-04-10 00:16:18 +0200

    validation

--- script.js
+++ script.js
@@ -2,26 +2,44 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+const form = document.getElementById('form');
 
 /// functions
 
+console.log('here now now')
+
+function showError(input,message) {
+    const manipulateErrorlateDiv = input.parentElement; 
+    manipulateErrorlateDiv.className = 'form-control error';
+    const small = manipulateErrorlateDiv.querySelector('small');
+    small.innerText = message;
+    
+
+};
+
+// Show success
+function showSuccess(input) {
+    const manipulateSuccesslateDiv = input.parentElement;
+    manipulateSuccesslateDiv.className = 'form-control success';
+};
 
 
+// Add events Listner
 
-// Add events Listnet
 
-form.addEventListner('submit', function(e) {
-    e.preventDefault()
+form.addEventListener('submit', function (e) {
 
-    console.log('submit')
+    e.preventDefault();
 
-    if( username === '') {
-        showError(username,'User Name is Required')
+    console.log('hdhdhdhdh')
+
+    if (username.value === '') {
+        showError(username, 'User name is Required')
     } else {
+
         showSuccess(username)
     }
-})
-
+});
 
 
 
